ae85f3dc62eaaf390ee349767f977035db0e4709
Nnamdi <emelunna@gmail.com>
2020-05-21 11:08:38 +0100

    implemented my_each_with_index and my_select methods

--- custom_enumerable_methods.rb
+++ custom_enumerable_methods.rb
@@ -19,6 +19,33 @@ def my_each
   end
 end
 
+def my_each_with_index 
+  return to_enum unless block_given?
+  self_class = self.class
+  x = 0
+  array = self_class == Array ? self : to_a
+    while x < array.length
+      yield array[x], x
+      x += 1
+    end 
+end
+
+def my_select
+  return to_enum unless block_given?
+  self_class = self.class
+
+  self_class == Array ? [] : {}
+    if self_class == Array
+        my_each do |x|
+        self_class.push(x) if yield(x)
+        end
+    else
+        my_each do |key, value|
+        self_class[key]  =  value) if yield (key, value)
+        end
+    end
+  self_class
+end
 
 
 
@@ -30,8 +57,8 @@ end
 
  array = [1, 2, 3, 4]
  aar = 1..5
- aar.each {|n| print n }
+ aar.each_with_index {|value, idx| puts "#{idx + 1} #{ value}"}
  puts
- aar.my_each {|n| print n }
+ aar.my_each_with_index {|value, idx| puts "#{idx + 1} #{ value}" }
  puts
 
