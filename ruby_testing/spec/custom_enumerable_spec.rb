fce92b15dac868563f9e1b0edc49c6529dd18139
maosan132 <maosan132@gmail.com>
2020-06-23 14:23:27 -0500

    finished my_all? testing and started my_any? testing.

--- ruby_testing/spec/custom_enumerable_spec.rb
+++ ruby_testing/spec/custom_enumerable_spec.rb
@@ -3,7 +3,7 @@ describe Enumerable do
   let(:test_array) { [5, 7, 9, 5] }
   let(:test_hash) { { w: 3, x: 4, y: 2, z: 6 } }
   let(:test_words) { %w[cat house tree fan] }
-  # let(:all_true)
+  let(:array_with_nil) { [5, nil, 9] }
   # let(:extended_class) { Class.new { extend Enumerable } }
   # w: 3, x: 4, y: 2, z: 6
 
@@ -36,19 +36,50 @@ describe Enumerable do
       expect(test_array.my_select).to be_a(Enumerable)
     end
     it "Returns an array with all elements for which the given block returns a true value" do
-      expect(test_array.my_select { |i| i.positive? }).to eql([5, 7, 9, 5])
+      expect(test_array.my_select { |i| i.positive? }).to eq([5, 7, 9, 5])
     end
     it "Returns an array with all even elements" do
       expect(test_array.my_select { |i| i.even? }).to eq([])
     end
-    it "should return an array with the value(s) which matches the condition on the block" do
+    it "should return an array with the value(s) which matches the condition within the block" do
       expect(test_words.my_select { |i| i == "house" }).to eq(["house"])
     end
   end
 
   describe "my_all" do
-    it "should return true if the block never returns false or nil" do
-      expect(test_words.all? { |i| i.length > 2 }).to eql(test_words.my_all? { |i| i.length > 2 })
+    it "should return true if the condition of the block never returns false or nil" do
+      expect(test_words.all? { |i| i.length > 2 }).to eq(test_words.my_all? { |i| i.length > 2 }) #compares to built-in #all?
+    end
+    it "should return true if all items match the condition within the block" do
+      expect(test_array.my_all? { |i| i.positive? }).to be(true)
+    end
+    describe "When a block is not given, ruby ads an implicit block of { |obj| obj }, and" do
+      it "should return true if none of the list items are false or nil" do
+        expect(test_array.my_all?).to eq(test_array.all?)
+      end
+      it "should return false if some item of the list i false or nil," do
+        expect(array_with_nil.my_all?).to eq(array_with_nil.all?)
+      end
+      it "should return true if all items match the RegExp condition" do
+        expect(test_words.all?(/t/)).to eq(test_words.my_all?(/t/)) #\d is used to find a tab character
+      end
+    end
+  end
+
+  describe "my_any" do
+    it "should return true if the condition of the block ever returns other value than false or nil" do
+      expect(test_words.my_any? { |i| i.length >= 5 }).to eq(true)
+    end
+    describe "When a block is not given, ruby ads an implicit block of { |obj| obj }, and" do
+      it "should return true if any of the list items are not false or nil" do
+        expect(array_with_nil.my_any?).to eq(false)  #there´s an error in enumerable_method code !!!!!!!!!!!!!!!!
+      end
+      it "should return false if some item of the list i false or nil," do
+        expect(array_with_nil.my_all?).to eq(array_with_nil.all?)
+      end
+      it "should return true if all items match the RegExp condition" do
+        expect(test_words.all?(/t/)).to eq(test_words.my_all?(/t/)) #\d is used to find a tab character
+      end
     end
   end
 end
